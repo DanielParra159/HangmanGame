0755a3032770df5715851712bed1e3773a2c3684
Daniel Parra <dani.parra.90@gmail.com>
2020-04-07 20:04:14 +0200

    Test

--- Assets/Scripts/Application/Services/GameServerService.cs
+++ Assets/Scripts/Application/Services/GameServerService.cs
@@ -25,6 +25,14 @@ namespace Application.Services
             _gameRepository.GameToken = response.token;
             return new Word(response.hangman);
         }
+		
+		public async Task<Word> StartNewGameTest()
+        {
+            var response = await _apiRest.Post<Request, NewGameResponse>("TODO: URL", new Request());
+            _gameRepository.Word = response.hangman;
+            _gameRepository.GameToken = response.token;
+            return new Word(response.hangman);
+        }
 
         [SuppressMessage("ReSharper", "InconsistentNaming")]
         public class NewGameResponse: Response
